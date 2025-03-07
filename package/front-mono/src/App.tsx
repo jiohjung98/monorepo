c4360847652058c3966717f7d838da2a50d50988
jiohjung98 <ckaquz98@naver.com>
2025-03-07 16:07:15 +0900

    feat: use common/index.ts in front-mono folder

--- package/front-mono/src/App.tsx
+++ package/front-mono/src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import str from '@monorepo/common'
 
 function App() {
   const [count, setCount] = useState(0)
+  console.log(str)
 
   return (
     <>
@@ -28,6 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <h1>{str}</h1>
     </>
   )
 }
